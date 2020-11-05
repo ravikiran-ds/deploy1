9a98c870c93ed5235353dd4693b6f63bf66e7398
Ravi Kiran Pendli <60742104+ravikiran-ds@users.noreply.github.com>
2020-11-05 10:27:19 +0530

    Add files via upload

--- portfolio.py
+++ portfolio.py
@@ -3,7 +3,11 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('base.html')
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
